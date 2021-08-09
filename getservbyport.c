0e93a0e73481c2add6a05e9a4017f6f409105357
Jacob Collins <jacobc1926@gmail.com>
2021-08-09 17:51:46 +1000

    Made custom port resolution, and added parity checking and whatnot for safety

---
+++ getservbyport.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+int main(int argc, char *argv[]) {
+	// Parse commmand line args
+	if(argc != 2) {
+		printf("ERROR:\tArgumental error\nUSAGE:\t%s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	// Parse port number in args, convert from char* to int
+	int portNumber = atoi(argv[1]);
+	if(portNumber < 1 || portNumber > 65535) {
+		printf("ERROR:\tInvalid port number: %s\nUSAGE:\t%s <port>\n", argv[1], argv[0]);
+		exit(2);
+	}
+
+	// Magic
+	struct servent *service = getservbyport(htons(portNumber), NULL);
+	if (service == NULL) {
+		printf("ERROR: I'm sorry idek\nUSAGE:\t%s <port>\n", argv[0]);
+		exit(3);
+	}
+
+	printf("%s:%s\n", argv[1], service->s_name);
+	return 0;
+}
